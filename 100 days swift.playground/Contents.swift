8c1c07d48ab699dc1156c738ba6f76fb0d2e05e6
mohanad sikkena <mo.sal5548@gmail.com>
2024-05-20 09:05:04 +0300

    second day

--- 100 days swift.playground/Contents.swift
+++ 100 days swift.playground/Contents.swift
@@ -2,18 +2,37 @@ import Cocoa
 
 
 
-var name:String="Ahmed Ali"
-print(name.hasPrefix("Ahmed"))
+let fileName="profile.jpg"
+print(fileName.hasSuffix(".jpg"))
 
-"""
-name variable has suffix Ali
-    \(name.hasSuffix("Ali"))
-"""
-var bigNumber=100_000
-bigNumber.isMultiple(of: 5)
-bigNumber%4==0
+let number=120
+print(number.isMultiple(of: 3))
 
-let doubleNumber=11.5
-let intNumber=10
-let intResult=intNumber-Int(doubleNumber)
-let doubleResult = Double(intNumber)-doubleNumber
+let isGoodDog=true
+var isGameOver=false
+let isMultiple=number.isMultiple(of: 2)
+isGameOver = !isGameOver
+isGameOver.toggle()
+isGameOver.toggle()
+
+// Join Strings Together
+
+let firstWord="First Word"
+let secondWord=" And Second Word Is Combined"
+let combined=firstWord + secondWord
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let longQuote = """
+Then he tapped a sign saying \"Believe\" and walked away.
+"""
+let name = "ahmed"
+let age = 16
+let nameAndAge="his name is \(name) and his age is \(age)"
+"5+5 = \(5+5)"
+
+
+// check Point 1
+
+let celsius=20
+let fahrenhiet=(celsius * 9 / 5) + 32
+print ("The current tempreture is \(celsius) celsuis and \(fahrenhiet) fahrenhiet°")
