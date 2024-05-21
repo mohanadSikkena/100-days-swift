7ae303eb6956b6309ba86baf6d1910518993eb31
mohanad sikkena <mo.sal5548@gmail.com>
2024-05-21 17:01:36 +0300

    day 4

--- 100 days swift.playground/Contents.swift
+++ 100 days swift.playground/Contents.swift
@@ -1,70 +1,26 @@
 import Cocoa
+//day 4 type annotations
+let surName : String = "Ali"
+var score : Int = 0
+let scores : Double=0
+let isAuthenticated : Bool = false
+let albums :[String]=["album1","Album2"]
+var user : [String:String] = ["name":"ahmed","age":"25","surName":"ali"]
+let books : Set<String> = ["ahmed in the river","ali in the river","ahmed in the river"]
+var cities : [String] = []
+var clues = [String]()
 
-// DDay 3
-var names = ["Ahmed","Mohammed","Ali","Mark"]
-let ages = [20,22,42,33]
-var tempretures=[15.5,22,30,38]
-
-names[0]
-ages[2]
-tempretures[3]
-names.append("Mostafa")
-
-var scores=Array<Int>()
-scores.append(5)
-scores.append(4)
-print(scores)
-
-var albums=Array<String>()
-albums.append("elemt")
-albums.append("element")
-print(albums)
-
-var albums2=[String]()
-albums2.append("album1")
-
-albums.count
-albums.remove(at: 1)
-print (albums)
-albums.removeAll()
-
-albums.contains("element")
-albums.append("element")
-albums.contains("element")
-
-var cities=["Cairo","Alabalma","Alfayoum","Alexandria","Giza"]
-cities.sort()
-print(cities)
-cities.reverse()
-print(cities)
-
-
-let employee=["name":"ahmed","jobTitle":"manager","salary":"2500"]
-print (employee["salary",default: "N/A"])
-print (employee["sal",default: "N/A"])
-
-var worldCup = [2022:"Qatar",2018:"Ruissa"]
-print(worldCup[2022,default: "N/A"])
-print(worldCup[2020,default: "N/A"])
-worldCup[2022]="cairo"
-print(worldCup[2022,default: "N/A"])
-
-let people=Set(["ahmed","mohammed","ali","ahmed","Ahmed"])
-print(people)
-var people2 = Set<String>()
-people2.insert("ali")
-people2.insert("Ali")
-people2.insert("Mohammed")
-people2.insert("Mohammed")
-print(people2)
-
-enum WeekDay{
-    case sunDay
-    case monDay,friDay
+enum UIStyle{
+    case light,dark,system
 }
+var style = UIStyle.dark
+style = .system
+let username:String
 
-var day=WeekDay.sunDay
-print(day)
-day = .monDay
-day = .friDay
+username="example"
 
+// checkpoint 2
+
+let array = ["ahmed","mohammed","ali","ali","mohammed"]
+print(array.count)
+let array2 : Set<String> = Set(array)
