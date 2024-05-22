6fb58ca6f084e9740c564c37fb40481c9c6ab852
mohanad sikkena <mo.sal5548@gmail.com>
2024-05-22 21:36:09 +0300

    day 5

--- 100 days swift.playground/Contents.swift
+++ 100 days swift.playground/Contents.swift
@@ -1,26 +1,151 @@
 import Cocoa
-//day 4 type annotations
-let surName : String = "Ali"
-var score : Int = 0
-let scores : Double=0
-let isAuthenticated : Bool = false
-let albums :[String]=["album1","Album2"]
-var user : [String:String] = ["name":"ahmed","age":"25","surName":"ali"]
-let books : Set<String> = ["ahmed in the river","ali in the river","ahmed in the river"]
-var cities : [String] = []
-var clues = [String]()
+//day 5
+let score = 85
 
-enum UIStyle{
-    case light,dark,system
+if score > 80 {
+    print(true)
 }
-var style = UIStyle.dark
-style = .system
-let username:String
 
-username="example"
+let age = 25
 
-// checkpoint 2
+if age > 18 {
+    print("Youre eligible to vote")
+}
 
-let array = ["ahmed","mohammed","ali","ali","mohammed"]
-print(array.count)
-let array2 : Set<String> = Set(array)
+let ourName="abcdefz"
+let friendName="abcdefg"
+
+if ourName > friendName {
+    print ("\(ourName) before \(friendName)")
+}
+
+if ourName < friendName {
+    print("\(friendName) befores \(ourName)")
+}
+
+var numbers = [1,2,3]
+numbers.append(4)
+
+if numbers.count > 3 {
+    numbers.removeFirst()
+}
+var userName = "King"
+if userName == "" {
+    userName="Anonymous"
+}
+
+if userName.count == 0 {
+    userName = "Anonymous"
+}
+if userName.isEmpty {
+    userName = "Anonymous"
+}
+print ("hello \(userName)")
+
+let userAge=18
+
+if userAge <= 18 {
+    print("you can vote")
+}else {
+    print ("you cant vote")
+}
+
+if userAge <= 18 {
+    print ("you can vote")
+}else if userAge == 17 {
+    print ("you need 1 year to vote")
+}else {
+    print ("you cant vote")
+}
+let temp = 25
+if temp > 20 && temp < 30 {
+    print("its a nice day")
+}
+let hasParentAgreement = false
+
+if hasParentAgreement || userAge >= 18 {
+    print ("You can Buy this game")
+}
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+let transport :TransportOption = .airplane
+
+if transport == .airplane || transport == .helicopter{
+    print ("lets fly")
+}else if transport == .bicycle {
+    print ("i hope theres a bike path")
+}else if transport == .car {
+    print("Time to get stuck in traffic.")
+}else {
+    print ("I will hire a scooter now")
+}
+
+enum Weather{
+    case sun , rain , snow ,wind , unknown
+}
+
+let forecast = Weather.snow
+
+switch forecast {
+case .sun :
+    print ("sunny")
+case .snow :
+    print("Snow")
+case  .rain :
+    print ("rain")
+case .wind :
+    print ("wind")
+case .unknown :
+    print ("Unknown")
+}
+
+
+let city = "cairo"
+switch city{
+case "alex" :
+    print("alex")
+case "cair" :
+    print("cair")
+default :
+    print (city)
+}
+
+let day = 4
+
+switch day {
+case 4 :
+    print (4)
+case 5 :
+    print(5)
+default :
+    print("default")
+}
+
+switch day {
+case 5 :
+    print("5 then ")
+    fallthrough
+case 4 :
+    print ("4 then ")
+    fallthrough
+case 3 :
+    print ("3")
+default :
+     print("default")
+}
+
+let canVote = age > 18 ? "Yes" : "No"
+
+
+let hour = 15
+
+print (hour > 12 ? "Its After Noon " : "Its before Noon" )
+
+enum Theme {
+    case light , dark , system
+}
+let theme = Theme.dark
+let background = theme == .dark ? "dark" : "Light or system"
+print(background)
+
