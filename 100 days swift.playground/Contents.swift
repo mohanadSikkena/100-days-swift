fc45b41a3dedfe647626c9607011ef5aab335916
mohanad sikkena <mo.sal5548@gmail.com>
2024-05-26 21:59:26 +0300

    day 8 and checkpoint 4

--- 100 days swift.playground/Contents.swift
+++ 100 days swift.playground/Contents.swift
@@ -1,90 +1,74 @@
 import Cocoa
-// day 7
+// day 8
 
-
-
-func showWelcome(){
-    print("Welcome to my app!")
-    print("By default This prints out a conversion")
-    print("chart from centimeters to inches, but you")
-    print("can also set a custom range if you want.")
-}
-showWelcome()
-
-func printTimesTable(number:Int){
-    for i in 1...12{
-        print("\(number) * \(i) = \(number*i)")
+func printTimesTables(for number: Int, end: Int = 12) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
     }
 }
-printTimesTable(number: 5)
-printTimesTable(number: 6)
 
-func printTimesTable2(number:Int , end:Int){
-    for i in 1...end{
-        print("\(number) * \(i) = \(number*i)")
+printTimesTables(for: 5, end: 10)
+printTimesTables(for: 5)
+
+enum Errors:Error {
+    case short , obvoius , noRoot , OutOfBounds
+}
+
+func checkPassword(_ password:String) throws ->String {
+    if(password.count < 8){
+        throw Errors.short
+    }else if(password=="12345678"){
+        throw Errors.obvoius
     }
+    if password.count < 10 {
+            return "OK"
+        } else if password.count < 12 {
+            return "Good"
+        } else {
+            return "Excellent"
+        }
 }
-printTimesTable2(number: 5,end: 7)
-
-func rollDice()->Int{
-    return Int.random(in: 1...6)
-}
-print(rollDice())
-
-func areLettersIdentical(word1:String,word2:String)->Bool{
-    return word1.sorted() == word2.sorted()
-}
-print(areLettersIdentical(word1: "asd", word2: "sda"))
-
-func rollDice2()->Int{
-    Int.random(in: 1...6)
-}
-
-func pythagoras(a:Double,b:Double)->Double{
-    sqrt(a*a + b*b)
-}
-print(pythagoras(a: 3, b: 4))
-
-
-func getUser()->[String]{
-    return ["ahmed" , "mohammed"]
+do {
+    let result = try checkPassword("12345678")
+    print(result)
+}catch Errors.short{
+    print("Error password iS short ")
+}catch Errors.obvoius{
+    print ("Error password is Obvious")
 }
 
-func getUser2()->[String:String]{
-    return ["name":"ahmed","secondName":"mohammed" ]
-}
+// checkpoint 4
+// a function that accepts an integer from 1 through 10,000, and returns the integer square root of that number. That sounds easy, but there are some catches:
+//If the number is less than 1 or greater than 10,000 you should throw an “out of bounds” error.
+//If you can’t find the square root, throw a “no root” error.
 
-func getUser3()->(firstName:String , secondName:String){
-    (firstName:"ahmed",secondName:"Mohammed")
-}
-print(getUser3().firstName)
-func getUser4()->(String ,String){
-    ("ahmed","Mohammed")
-}
-print(getUser4().0)
-let (firstName,secondName)=getUser3()
-print(firstName)
 
-func rollDiceUpdated(sides:Int,count:Int)->[Int]{
-    var rolls=[Int]()
-    for _ in 1...count{
-        rolls.append(Int.random(in: 1...sides))
+func getSqrt(_ number:Int) throws ->Int{
+    var solution :Int = 0
+    if (number > 10_000 || number < 1){
+        throw Errors.OutOfBounds
     }
-    return rolls
+    if (number == 1){
+        solution = 1
+        return solution
+    }
+    for i in 1...100 {
+        if (i * i == number) {
+            solution = i
+            break
+        }
+    }
+    if(solution==0){
+        throw Errors.noRoot
+    }
+    return solution
+    
 }
-print (rollDiceUpdated(sides: 6, count: 10))
-
-func rollDice(sides:Int,count:Int)->Int{
-    return Int.random(in: 1...6)
+do{
+    let solution = try getSqrt(10000)
+    print(solution)
+}catch Errors.noRoot{
+    print("no root")
+}catch Errors.OutOfBounds{
+    print ("Out of bounds")
 }
-print(rollDice(sides: 6, count: 10))
-
-func isUpperCased(string:String)->Bool{
-    string==string.uppercased()
-}
-print(isUpperCased(string: "ahmed"))
-func isUpperCased(_ string:String)->Bool{
-    string==string.uppercased()
-}
-print(isUpperCased("AHMED"))
-
