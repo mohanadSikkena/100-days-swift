355d0632fb3efa6e2feb87f3d0ae404e8fcd1d3d
mohanad sikkena <mo.sal5548@gmail.com>
2024-05-26 18:27:55 +0300

    day 7

--- 100 days swift.playground/Contents.swift
+++ 100 days swift.playground/Contents.swift
@@ -1,85 +1,90 @@
 import Cocoa
-// day 6
-let platforms = ["macOs","Ios","TvOs"]
+// day 7
 
-for device in platforms {
-    print ("swift works fine on \(device)")
-}
 
-for i in 1...10 {
-    print ("\(i) * 5 = \(i*5)")
+
+func showWelcome(){
+    print("Welcome to my app!")
+    print("By default This prints out a conversion")
+    print("chart from centimeters to inches, but you")
+    print("can also set a custom range if you want.")
 }
-for i in 1...10 {
-    for j in 1...10 {
-        print ("\(i) * \(j) = \(i*j)")
+showWelcome()
+
+func printTimesTable(number:Int){
+    for i in 1...12{
+        print("\(number) * \(i) = \(number*i)")
     }
 }
+printTimesTable(number: 5)
+printTimesTable(number: 6)
 
-for i in 1...5 {
-    print("this count from 1 to 5 \(i)")
-}
-for i in 1..<5 {
-    print ("this count from 1 to 4 \(i)")
-}
-
-var lyric = "Haters Gonna "
-
-for _ in 1...5 {
-    lyric += "hate  "
-}
-print(lyric)
-
-var countdown = 10
-while countdown > 0 {
-    print (countdown)
-    countdown -= 1
-}
-
-var roll = 0
-
-while roll != 25 {
-    
-    roll = Int.random(in: 1...100)
-    print ("I rolled \(roll)")
-}
-print ("Baaam")
-
-
-let fileNames = ["a.jpg" , "b.txt" , "c.exe", "d.jpg" , "s.json"]
-
-for fileName in fileNames {
-    if !fileName.hasSuffix(".jpg"){
-        continue
-    }
-    print("found image \(fileName)")
-}
-
-
-let number1 = 5
-let number2 = 24
-var multiples = [Int]()
-
-for i in 1...100_000{
-    if i.isMultiple(of: number1) && i.isMultiple(of: number2){
-        multiples.append(i)
-        if multiples.count >= 10 {
-            break
-        }
+func printTimesTable2(number:Int , end:Int){
+    for i in 1...end{
+        print("\(number) * \(i) = \(number*i)")
     }
 }
-print(multiples)
+printTimesTable2(number: 5,end: 7)
 
-
-// Chek point 3
-
-for i in 1...100 {
-    if i.isMultiple(of: 3) && i.isMultiple(of: 5){
-        print("FizzBuzz")
-    }else if i.isMultiple(of: 3){
-        print("Fizz")
-    }else if i.isMultiple(of: 5){
-        print("Buzz")
-    }else {
-        print(i)
-    }
+func rollDice()->Int{
+    return Int.random(in: 1...6)
 }
+print(rollDice())
+
+func areLettersIdentical(word1:String,word2:String)->Bool{
+    return word1.sorted() == word2.sorted()
+}
+print(areLettersIdentical(word1: "asd", word2: "sda"))
+
+func rollDice2()->Int{
+    Int.random(in: 1...6)
+}
+
+func pythagoras(a:Double,b:Double)->Double{
+    sqrt(a*a + b*b)
+}
+print(pythagoras(a: 3, b: 4))
+
+
+func getUser()->[String]{
+    return ["ahmed" , "mohammed"]
+}
+
+func getUser2()->[String:String]{
+    return ["name":"ahmed","secondName":"mohammed" ]
+}
+
+func getUser3()->(firstName:String , secondName:String){
+    (firstName:"ahmed",secondName:"Mohammed")
+}
+print(getUser3().firstName)
+func getUser4()->(String ,String){
+    ("ahmed","Mohammed")
+}
+print(getUser4().0)
+let (firstName,secondName)=getUser3()
+print(firstName)
+
+func rollDiceUpdated(sides:Int,count:Int)->[Int]{
+    var rolls=[Int]()
+    for _ in 1...count{
+        rolls.append(Int.random(in: 1...sides))
+    }
+    return rolls
+}
+print (rollDiceUpdated(sides: 6, count: 10))
+
+func rollDice(sides:Int,count:Int)->Int{
+    return Int.random(in: 1...6)
+}
+print(rollDice(sides: 6, count: 10))
+
+func isUpperCased(string:String)->Bool{
+    string==string.uppercased()
+}
+print(isUpperCased(string: "ahmed"))
+func isUpperCased(_ string:String)->Bool{
+    string==string.uppercased()
+}
+print(isUpperCased("AHMED"))
+
