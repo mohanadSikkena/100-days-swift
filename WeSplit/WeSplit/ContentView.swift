f35d5e84a30936a5f8455d05e7845d587938ad4e
mohanad sikkena <mo.sal5548@gmail.com>
2024-05-30 13:32:43 +0300

    project 1 WeSplit day 16 - 18

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -24,124 +24,73 @@ struct ContentView: View {
 //    ContentView()
 //}
 
-struct FormView: View {
-    var body : some View {
-        Form{
-            Text("Hello Form")
-            Text("Hello Form")
-            Text("Hello Form")
-            Text("Hello Form")
-            Text("Hello Form")
-            Text("Hello Form")
-            Text("Hello Form")
-            Text("Hello For m")
-            Section{
-                Text("Hello Section ") 
-                Text("Hello Section ")
-                Text("Hello Section ")
-                Text("Hello Section ")
-                
-            }
-            Section{
-                Text("Hello Section ")
-                Text("Hello Section ")
-                Text("Hello Section ")
-                Text("Hello Section ")
-                Text("Hello Section ")
-                Text("Hello Section ")
-                Text("Hello Section ")
-            }
-        }
+struct WeSplitContentView : View{
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    private let tipPercentages = [0 , 10 , 15 , 20 , 25 , 30 ]
+    @FocusState private var amountFocus : Bool
+    
+    var totalCheck : Double{
+        let tipSelection = Double(tipPercentage)
+        
+        let checkTipAmount = tipSelection / 100.0 * checkAmount
+        let grandAmount = checkAmount + checkTipAmount
+        return grandAmount
     }
-}
-//#Preview{
-//    FormView()
-//}
-
-struct Navigation : View {
-    var body : some View {
-        NavigationStack {
-            Form {
-                Section {
-                    Text("Hello Navigation Stack")
-                }
-            }
-            .navigationTitle("Hello SwiftUi")
-            .navigationBarTitleDisplayMode(.inline)
-        }
+    var totalPerson : Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        
+        let amountPerPerson = totalCheck / peopleCount
+        
+        return amountPerPerson
     }
-}
-//#Preview {
-//    Navigation()
-//}
-
-struct ButtonTest : View {
-    @State private var tapCount : Int = 0
-    var body: some View {
-        Button("Tap count \(self.tapCount)") {
-            self.tapCount += 1
-        }
-    }
-}
-//#Preview{
-//    ButtonTest()
-//}
-
-struct TextBox : View {
-    @State private var name = ""
-    var body: some View {
-        Form{
-            TextField("Enter Yor Name" , text: $name)
-            Text("Hello \(name)")
-            Button("Change Name"){name = "No Name"}
-        }
-    }
-}
-
-//#Preview{
-//    TextBox()
-//}
-
-struct LoopViews : View {
-    var body: some View {
-        Form{
-            ForEach(0..<100){
-                Text("The Current Value Is \($0)")
-            }
-        }
-    }
-}
-//#Preview{
-//    LoopViews()
-//}
-
-struct PickerView : View {
-    @State private var students=["Ahmed" , "Mohammed" , "Ali"]
-    @State private var selectedStudent = "Ahmed"
-    @State private var newStudent=""
     var body: some View {
         NavigationStack{
             Form{
-                Picker("Select Your Student" ,selection: $selectedStudent){
-                    ForEach(students, id: \.self){
-                        Text($0)
-                    }
-                }
-                Section{
-                    TextField("New Student",text: $newStudent)
-                    Button("Add Student"){
-                        if !newStudent.isEmpty {
-                            students.append(newStudent)
-                            newStudent = ""
+                Section {
+                    TextField("Amount" ,value: $checkAmount ,format: .currency(code:Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad).focused($amountFocus)
+                    Picker("Number Of People" , selection: $numberOfPeople){
+                        ForEach (2..<100){
+                            Text("\($0) People")
                         }
-                    }
+                    }.pickerStyle(.navigationLink)
+                }
+                Section("How Much Do You want To Tip?"){
+                
+                    Picker("Tip percentage" , selection: $tipPercentage){
+                        ForEach(tipPercentages , id: \.self){
+                            Text($0 , format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
                 }
                 
+                Section("How Much Do You want To Tip?"){
+                
+                    Picker("Tip percentage" , selection: $tipPercentage){
+                        ForEach(0..<101 , id: \.self){
+                            Text($0 , format: .percent)
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                Section ("Amount per person"){
+                    Text(totalPerson , format: .currency(code:Locale.current.currency?.identifier ?? "USD"))
+                }
+                Section("Total Check"){
+                    Text(totalCheck , format: .currency(code:Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+            }.navigationTitle("WeSplit").toolbarTitleDisplayMode(.inline).toolbar{
+                if amountFocus {
+                    Button("Done"){
+                        amountFocus = false
+                    }
+                }
             }
-            
-        }
+
+        }   
     }
 }
 #Preview{
-    PickerView()
+    WeSplitContentView()
 }
