d0519cb7c86de3f4110adafe76020c51c11d130c
mohanad sikkena <mo.sal5548@gmail.com>
2024-05-30 14:43:20 +0300

    Temperature conversion App

---
+++ challenge1/challenge1/challenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  challenge1App.swift
+//  challenge1
+//
+//  Created by mohanad sikkena on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct challenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
