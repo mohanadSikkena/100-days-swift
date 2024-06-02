e9a544e607a5906f9af5d2f029be63c657a7ffc5
mohanad sikkena <mo.sal5548@gmail.com>
2024-06-02 15:42:20 +0300

    Guess The Flag Full Game Day 22

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,56 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     @State private var showAlert = false
     @State private var correctAnswer = Int.random(in: 0...2)
-    @State var pressed:Int?
-    @State var streak = 0
-    var isCorrectAnswer :Bool{
-        correctAnswer == pressed
+    @State private var score = 0
+    @State private var scoreTitle = ""
+    @State private var scoreMessage=""    
+    func newGame(){
+        correctAnswer = Int.random(in: 0...2)
+        countries.shuffle()
+    }
+    
+    
+    func buttonPressed(pressedNumber:Int){
+        if pressedNumber == correctAnswer{
+            scoreTitle = "Correct"
+            score += 1
+            scoreMessage = "Your Score Is \(score)"
+        }else{
+            scoreMessage = "Your Score Is \(score)"
+            scoreTitle = "Wrong, That’s The Flag Of \(countries[pressedNumber])"
+            score = 0
+        }
+        showAlert = true
     }
     var body: some View {
 
 
         ZStack{
   
-            Color.blue.ignoresSafeArea()
-            VStack(spacing:30){
-                VStack{
-                    Text("Tap The Flag of \(countries[correctAnswer])").foregroundStyle(.white)
+            CustomGradiant()
+            VStack{
+                Spacer()
+                Spacer()
+                Text("Guess The Flag").font(.largeTitle.weight(.bold))
+                    .foregroundStyle(.white)
+                VStack(spacing:15){
+                    VStack{
+                        Text("Tap The Flag of").foregroundStyle(.secondary).font(.subheadline.weight(.heavy))
+                        Text("\(countries[correctAnswer])").font(.largeTitle.weight(.semibold))
 
-                }
-                    ForEach(0..<3){number in
-                        Button{
-                            pressed = number
-                            if isCorrectAnswer {
-                                streak += 1
+                    }
+                        ForEach(0..<3){number in
+                            Button{
+                                buttonPressed(pressedNumber: number)
+                                
+                            }label:{
+                                Image(countries[number]).clipShape(.capsule).shadow(radius: 5)
                             }
-                            showAlert = true
-                        }label:{
-                            Image(countries[number])
-                        }
-                    
-            }
-            }
-        }.alert(isCorrectAnswer ? "Winner " : "Ooops!",isPresented: $showAlert){
-            
-            Button("Next Game"){
-                correctAnswer = Int.random(in: 0...2)
-                countries.shuffle()
-                if !isCorrectAnswer {
-                    streak = 0
+                        
                 }
-                
-            }
-            Button("Replay"){
-                streak = 0
-            }
+                }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .padding(.vertical,20)
+                    .background(.regularMaterial)
+                    .clipShape(.rect(cornerRadius: 20))
+                Spacer()
+                Text("Score : \(score)")
+                    .font(.title.bold())
+                    .foregroundStyle(.white)
+                Spacer()
+                Spacer()
+            }.padding()
+        }.alert(scoreTitle,isPresented: $showAlert){
+            
+            Button("Next Game",action: newGame)
+            
         }message: {
-            Text("Winning Streak \(streak)")
-        }
+            Text(scoreMessage)
+            
+        }.ignoresSafeArea()
     }
 }
 #Preview {
     ContentView()
+}
+
+struct CustomGradiant :View {
+    var body: some View {
+        RadialGradient(stops: [
+            .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+            .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+           ], center: .top, startRadius: 200, endRadius: 700)
+    }
 }
